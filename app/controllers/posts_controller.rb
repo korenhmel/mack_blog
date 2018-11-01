c0dab59994dd34a1fbb9079c2e41b34e47527e71
koren <koren.hmel@gmail.com>
2018-11-01 13:52:58 +0200

    add model post and views: new.html and chow.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,37 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
  def index
+  @posts = Post.all
   end
 
   def show
+   
   end
 
   def new
+    @post = Post.new
   end
 
-  def create  
+  def create 
+   @post = Post.new(post_params)
+   if @post.save
+    redirect_to post_url(@post)
+  else 
+    render :new
+  end
+
   end
 
   def update
   end
 
   def destroy
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
